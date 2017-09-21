d23045acb2be223fdc571c8180034d45ca27ae50
SoPudge <abcd666@gmail.com>
2017-09-21 08:35:05 +0800

    循»习1

---
+++ class5/forall.java
@@ -0,0 +1,15 @@
+public class forall{
+    public static void main (String[] args) {
+        int num = 20;
+        int gold = 0;
+        for (int a = 1;a <= num/2;a++){
+            for (int b = num;b >= num/2;b--){
+                if (a % 2 ==0 & b %  == 0)
+                    continue;
+                double lastgold = gold;
+                gold = (double) (a / b);
+                if (0.618 - lastgold) >
+            }
+        }
+    }
+}
